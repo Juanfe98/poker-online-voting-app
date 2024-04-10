5769785b2687c5df93e1fb25bf2e49bcb04c48f8
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2024-04-10 00:10:16 -0500

    Saving the current user information in the zustand store

--- src/components/pages/user-join/index.tsx
+++ src/components/pages/user-join/index.tsx
@@ -4,32 +4,37 @@ import {
   Flex,
   FormControl,
   FormLabel,
-  Input,
+  Input
 } from '@chakra-ui/react'
+import { v4 as uuidv4 } from 'uuid'
 import { useZustandState } from '../../store'
 
 function UserJoin() {
-  const addTeamMember = useZustandState(state => state.addTeamMember)
+  const setCurrentUser = useZustandState(state => state.setCurrentUser)
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const form = event.currentTarget
     const nameInput = form.elements.namedItem('user-name') as HTMLInputElement
-    addTeamMember({ name: nameInput.value })
+    const currentUser = {
+      id: uuidv4(),
+      name: nameInput.value
+    }
+    setCurrentUser(currentUser)
   }
 
   return (
     <Flex height="90vh" alignItems="center" justifyContent="center">
-        <Box maxW="md" w="sm" h="xxs" p={6} boxShadow="2xl" borderRadius="lg">
-          <form onSubmit={handleSubmit}>
-            <FormControl>
-              <FormLabel>Name: </FormLabel>
-              <Input type="text" name="user-name" />
-            </FormControl>
-            <Button mt={5} type="submit">
-              Continue
-            </Button>
-          </form>
-        </Box>
+      <Box maxW="md" w="sm" h="xxs" p={6} boxShadow="2xl" borderRadius="lg">
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <FormLabel>Name: </FormLabel>
+            <Input type="text" name="user-name" />
+          </FormControl>
+          <Button mt={5} type="submit">
+            Continue
+          </Button>
+        </form>
+      </Box>
     </Flex>
   )
 }
