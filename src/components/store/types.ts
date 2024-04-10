e026dbcff7cd5b0445c4f5cd2cad9bf925b13b6b
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2024-04-10 00:10:39 -0500

    Create interface for currentUser

--- src/components/store/types.ts
+++ src/components/store/types.ts
@@ -2,4 +2,9 @@ export interface TeamMember {
   name: string
 }
 
+export interface CurrentUser {
+  id: string;
+  name: string;
+}
+
 export type PointingType = 'fibonacci' | 'powersOfTwo' | 'tShirtSizing' | 'smallValues'
